f51278d1274399e548bcdc42397cf76e030b221d
zhongshuai <zhongshuai@megvii.com>
2019-03-12 18:56:11 +0800

    菜单实现可配置

--- src/page/ largeCreen/index.vue
+++ src/page/ largeCreen/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Description: 
+ * @Author: zhongshuai
+ * @LastEditors: zhongshuai
+ * @Date: 2019-03-12 15:11:20
+ * @LastEditTime: 2019-03-12 18:56:04
+ -->
 <template>
   <div>large</div>
 </template>
