b6aa19ac722ade49b80962cfee3e68bee5bfab77
zhongshuai <zhongshuai@megvii.com>
2019-03-15 11:30:26 +0800

    xl_close_port

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 # 当发生错误时中止脚本
 # set -e
+# cnpm install serve
+# cnpm install -g xl_close_port
 
 # 删除日志
 rm -rf nohup.out 
@@ -18,6 +20,9 @@ npm run build
 
 # cd 到构建输出的目录下 
 cd dist
+
+#关闭之前的
+xl_close_port -p 5000
 
 # 启动
 serve -l 5000 &
