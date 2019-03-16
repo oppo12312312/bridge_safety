fe45c7699ecbaef0239e82170fb8e89384f9ccd0
zhongshuai <zhongshuai@megvii.com>
2019-03-16 20:34:05 +0800

    兼容

--- src/page/ largeCreen/card.vue
+++ src/page/ largeCreen/card.vue
@@ -3,14 +3,14 @@
  * @Author: zhongshuai
  * @LastEditors: zhongshuai
  * @Date: 2019-03-13 18:04:56
- * @LastEditTime: 2019-03-13 20:01:02
+ * @LastEditTime: 2019-03-16 20:29:55
  -->
 <template>
   <div class="br-card"> 
-    <p class="title">
+    <div class="title">
       天下无贼
-    </p>
-    <p class="text"><span class="namber">254</span> <span class="unit">天</span></p>
+    </div>
+    <div class="text"><span class="namber">254</span> <span class="unit">天</span></div>
   </div>
 </template>
 
