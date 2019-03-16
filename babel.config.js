8a6f8e31b70d44777ed79faf21e95e7eb4e3c713
zhongshuai <zhongshuai@megvii.com>
2019-03-16 16:18:19 +0800

    添加lable node_modules

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@
  * @Author: zhongshuai
  * @LastEditors: zhongshuai
  * @Date: 2019-03-11 16:31:30
- * @LastEditTime: 2019-03-11 19:30:55
+ * @LastEditTime: 2019-03-16 16:02:53
  */
 module.exports = {
   presets: [
--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,13 @@
  * @Author: zhongshuai
  * @LastEditors: zhongshuai
  * @Date: 2019-03-11 19:29:28
- * @LastEditTime: 2019-03-12 14:46:03
+ * @LastEditTime: 2019-03-16 16:02:52
  */
 module.exports = {
+  transpileDependencies: [
+    'node_modules/leaflet',
+    'node_modules/echarts'
+  ],
   css: {
     loaderOptions: {
       sass: {
