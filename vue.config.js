255552dd0454a5321cb226cee4323b3be0707e88
zhongshuai <zhongshuai@megvii.com>
2019-03-16 17:32:49 +0800

    url去掉#

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,14 @@
  * @Author: zhongshuai
  * @LastEditors: zhongshuai
  * @Date: 2019-03-11 19:29:28
- * @LastEditTime: 2019-03-16 16:02:52
+ * @LastEditTime: 2019-03-16 17:31:31
  */
 module.exports = {
   transpileDependencies: [
     'node_modules/leaflet',
-    'node_modules/echarts'
+    'node_modules/leaflet',
+    'node_modules/v-charts',
+    'node_modules/vue2-leaflet'
   ],
   css: {
     loaderOptions: {
