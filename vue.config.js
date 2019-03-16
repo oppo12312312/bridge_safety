9ba96189026672a8ef972ffb0a35c34d933d4486
zhongshuai <zhongshuai@megvii.com>
2019-03-16 20:15:17 +0800

    promis

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
  * @Author: zhongshuai
  * @LastEditors: zhongshuai
  * @Date: 2019-03-11 19:29:28
- * @LastEditTime: 2019-03-16 19:56:41
+ * @LastEditTime: 2019-03-16 20:14:05
  */
 module.exports = {
   transpileDependencies: [
@@ -12,6 +12,9 @@ module.exports = {
     'node_modules/v-charts',
     'node_modules/vue2-leaflet'
   ],
+  configureWebpack: {
+    entry: ['babel-polyfill', './src/main.js']
+  },
   css: {
     loaderOptions: {
       sass: {
