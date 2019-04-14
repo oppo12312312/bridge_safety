37fecba3f2a5026569712bd3bc1a6e9d4ecafc71
zhongshuai <zhongshuai@megvii.com>
2019-04-14 20:11:46 +0800

    cesshi

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
  * @Author: zhongshuai
  * @LastEditors: zhongshuai
  * @Date: 2019-03-11 16:31:30
- * @LastEditTime: 2019-04-14 20:00:26
+ * @LastEditTime: 2019-04-14 20:11:18
  -->
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view>  </router-view>
   </div>
 </template>
 
